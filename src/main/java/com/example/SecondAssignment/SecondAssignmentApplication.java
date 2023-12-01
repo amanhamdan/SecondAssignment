9d80fbe3e23dfea09245261457832f75156bf4ac
amanhamdan <aman.hamdan@contractoralefeducation.com>
2023-12-01 21:25:38 +0400

    first commit message

---
+++ src/main/java/com/example/SecondAssignment/SecondAssignmentApplication.java
@@ -0,0 +1,14 @@
+package com.example.SecondAssignment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecondAssignmentApplication {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World !!! - change in feature and another 11");
+		//SpringApplication.run(SecondAssignmentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/SecondAssignment/SecondAssignmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SecondAssignment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecondAssignmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
